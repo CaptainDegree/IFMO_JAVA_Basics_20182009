5de8de0a99508a1cf56e6c9c3279649642fbb818
Ildar <renorq@mail.ru>
2018-10-05 12:01:37 +0300

    job's done task 3

--- src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
+++ src/main/java/ru/ifmo/cet/javabasics/WarAndPeaceExercise.java
@@ -1,21 +1,43 @@
 package ru.ifmo.cet.javabasics;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import static java.nio.file.Files.readAllLines;
 
 public class WarAndPeaceExercise {
 
-    public static String warAndPeace() {
+    public static String warAndPeace() throws IOException {
         final Path tome12Path = Paths.get("src", "main", "resources", "WAP12.txt");
         final Path tome34Path = Paths.get("src", "main", "resources", "WAP34.txt");
 
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO Iff word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order iff needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        Map<String, Integer> wordsAndAmounts = new HashMap<>();
 
-        throw new UnsupportedOperationException();
+        String text = String.join(" ", readAllLines(tome12Path, Charset.forName("windows-1251")));
+        text += String.join(" ", readAllLines(tome34Path, Charset.forName("windows-1251")));
+
+        Stream.of(text.replaceAll("[^a-zA-Zа-яА-Я]", " ").toLowerCase().split(" "))
+                .filter(word -> word.length() >= 4)
+                .forEach(word ->
+                        wordsAndAmounts.put(word,
+                                wordsAndAmounts.getOrDefault(word, 0) + 1));
+
+        Stream<Map.Entry<String, Integer>> strm = wordsAndAmounts.entrySet().stream()
+                .sorted((e1, e2) ->
+                        (e1.getValue().equals(e2.getValue())) ?
+                                e1.getKey().compareTo(e2.getKey()) :
+                                e2.getValue().compareTo(e1.getValue()))
+                .filter(e -> e.getValue() >= 10);
+
+        StringBuilder ans = new StringBuilder();
+
+        strm.forEach(e -> ans.append(e.getKey()).append(" - ").append(e.getValue()).append("\n"));
+
+        return ans.substring(0, ans.length() - 1);
     }
-
 }
